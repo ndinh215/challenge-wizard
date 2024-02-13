5dbfed2beb7e941b673a81ca8d5a1ca8e97d7d1f
nghia.dinh <blackjackptit@gmail.com>
2024-02-13 10:49:47 +0700

    Add testing for exception handling.

---
+++ src/test/java/com/rapiddweller/services/ServiceExceptionHandlerTest.java
@@ -0,0 +1,10 @@
+package com.rapiddweller.services;
+
+import org.junit.jupiter.api.Test;
+
+public class ServiceExceptionHandlerTest {
+    @Test
+    void testHandleAccessDeniedException() {
+
+    }
+}
